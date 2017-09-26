82ea8b2338ee51d9c9d4843e9cc0e852c4c4989a
Antoine Carme <antoine.carme@laposte.net>
2017-09-26 21:03:24 +0200

    Add a config #6

--- explainer.py
+++ explainer.py
@@ -1,4 +1,5 @@
-from sklearn_explain.reason_codes import explain_class as expl
+from sklearn_explain.reason_codes import explain_factory as expl
+from sklearn_explain.reason_codes import settings as conf
 
 # This is a model explainer designed for classifiers and regressors with continous inputs.
 # Support for multiclass models and categorical inputs is coming.
@@ -15,11 +16,13 @@ class cModelScoreExplainer:
     def __init__(self , clf):
         self.mModel = clf
         self.mImplemantation = None
+        self.mSettings = conf.cScoreExplainerConfig()
         self.mDebug = True
 
 
     def fit(self, X):
-        self.mImplemantation = expl.cClassificationModel_ScoreExplainer(self.mModel)
+        lFactory = expl.cScoreExplainerFactory()
+        self.mImplemantation = lFactory.build_Explainer(self.mModel , self.mSettings)
         if(hasattr(self , "mFeatureNames")):
             self.mImplemantation.mFeatureNames = self.mFeatureNames
         return self.mImplemantation.fit(X)
