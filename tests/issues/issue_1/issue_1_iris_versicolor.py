cb02b6ad3567f588baefb96836bece930c39816d
Antoine Carme <antoine.carme@laposte.net>
2017-09-27 18:22:52 +0200

    Add Multiclass Models support #1
    Analysis scripts

---
+++ tests/issues/issue_1/issue_1_iris_versicolor.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+import pandas as pd
+import sklearn_explain.explainer as expl
+
+ds = datasets.load_iris();
+print(ds.__dict__)
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=12, random_state = 1960)
+
+clf.fit(ds.data , ds.target)
+
+lExplainer = expl.cModelScoreExplainer(clf)
+lExplainer.mSettings.mFeatureNames = ds.feature_names
+lExplainer.mSettings.mScoreBins = 10
+lExplainer.mSettings.mFeatureBins = 10
+lExplainer.mSettings.mMaxReasons = 2
+lExplainer.mSettings.mExplanationOrder = 1
+lExplainer.mSettings.mClasses = ds.target_names # ['setosa', 'versicolor', 'virginica']
+lExplainer.mSettings.mMainClass = 'versicolor'
+lExplainer.fit(ds.data)
+df_rc = lExplainer.explain(ds.data)
+
+print(df_rc.head())
