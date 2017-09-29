790d37ea90950d80f0abb6e864cf08096ee0a28d
Antoine Carme <antoine.carme@laposte.net>
2017-09-29 13:22:19 +0200

    Added a build-test target for travis-ci tests

--- tests/gen_makefile.py
+++ tests/gen_makefile.py
@@ -3,8 +3,10 @@ import glob
 
 subdirs = ["tests/protoyping"] +  glob.glob("tests/skl_datasets/*") +  glob.glob("tests/skl_datasets_reg/*") +  glob.glob("tests/issues/*") 
 
+build_tests = ['sample', 'tests_issues', 'iris', 'boston']
 
 all_target = "";
+build_target = "";
 for subdir1 in subdirs:
     test_target = "";
     for filename in glob.glob(subdir1 + "/*.py"):
@@ -15,15 +17,19 @@ for subdir1 in subdirs:
         logfile = bn.replace("/" , "_");
         logfile = "logs/" + logfile.replace(".py" , ".log");
         if(lKeep):
-            print(bn1 + ": " , "\n\t", "-python3" , filename , " > " , logfile , " 2>&1");
+            print(bn1 + ": " , "\n\t", "-python3" , filename , " > " , logfile , " 2>&1\n");
         test_target = bn1 + " " + test_target;
 
     subdir1_label = subdir1.replace("/" , "_")
     print(subdir1_label + ":" , test_target)
     all_target = all_target + " " + subdir1_label;
+    for bt in build_tests:
+        if(bt in subdir1_label):
+            build_target = build_target + " " + subdir1_label;
 
 #print("\n# ********************************************** \n");
 
-print("all:  " , all_target, "\n\t\n");
+print("\n\nall:  " , all_target, "\n\t\n");
+print("\n\nbuild-test:  " , build_target, "\n\t\n");
 
 
