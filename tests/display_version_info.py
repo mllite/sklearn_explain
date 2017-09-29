812c52a84c73717ecfa7edc8dd17e355afdf3341
Antoine Carme <antoine.carme@laposte.net>
2017-09-29 11:48:21 +0200

    Add a basic test framework #4
    Added a script to get versions of all needed packages

---
+++ tests/display_version_info.py
@@ -0,0 +1,27 @@
+
+def getVersions():
+        
+    import os, platform
+    lVersionDict = {};
+    lVersionDict["system_platform"] = platform.platform();
+    lVersionDict["system_uname"] = platform.uname();
+    lVersionDict["system_processor"] = platform.processor();
+    lVersionDict["python_implementation"] = platform.python_implementation();
+    lVersionDict["python_version"] = platform.python_version();
+    
+    import sklearn
+    lVersionDict["sklearn_version"] = sklearn.__version__;
+    
+    import pandas as pd
+    lVersionDict["pandas_version"] = pd.__version__;
+    
+    import numpy as np
+    lVersionDict["numpy_version"] = np.__version__;
+    
+    import scipy as sc
+    lVersionDict["scipy_version"] = sc.__version__;
+    
+    print([(k, lVersionDict[k]) for k in sorted(lVersionDict)]);
+    return lVersionDict;
+    
+getVersions()
