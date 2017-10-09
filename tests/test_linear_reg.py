f21ba2f135fe215a1651359610ec1cf72865b77b
Antoine Carme <antoine.carme@laposte.net>
2017-10-09 10:51:24 +0200

    Corrected a typo

--- tests/test_linear_reg.py
+++ tests/test_linear_reg.py
@@ -2,23 +2,23 @@ from sklearn_explain.tests.skl_datasets_reg import skl_datasets_test as skltest
 
 
 lExplainer = skltest.test_reg_dataset_and_model("RandomReg_10" , "Ridge_6")
-print(lExplainer.mImplemantation.__dict__)
+print(lExplainer.mImplementation.__dict__)
 
-print(lExplainer.mImplemantation.mUsedFeatureNames)
-print(lExplainer.mImplemantation.mScoreQuantiles)
-print(lExplainer.mImplemantation.mFeatureQuantiles)
-print(lExplainer.mImplemantation.mScoreBinInterpolators)
-print(lExplainer.mImplemantation.mScoreBinInterpolationIntercepts)
-print(lExplainer.mImplemantation.mContribMeans)
-print(lExplainer.mImplemantation.mExplanations)
-print(lExplainer.mImplemantation.mExplanationData)
-print(lExplainer.mImplemantation.mCategories)
+print(lExplainer.mImplementation.mUsedFeatureNames)
+print(lExplainer.mImplementation.mScoreQuantiles)
+print(lExplainer.mImplementation.mFeatureQuantiles)
+print(lExplainer.mImplementation.mScoreBinInterpolators)
+print(lExplainer.mImplementation.mScoreBinInterpolationIntercepts)
+print(lExplainer.mImplementation.mContribMeans)
+print(lExplainer.mImplementation.mExplanations)
+print(lExplainer.mImplementation.mExplanationData)
+print(lExplainer.mImplementation.mCategories)
 
-coeffs = lExplainer.mImplemantation.mScoreBinInterpolationCoefficients
+coeffs = lExplainer.mImplementation.mScoreBinInterpolationCoefficients
 for k in coeffs:
     print(k, coeffs[k])
 
 
-for k in lExplainer.mImplemantation.mFeatureQuantiles:
-    print(k, lExplainer.mImplemantation.mFeatureQuantiles[k])
+for k in lExplainer.mImplementation.mFeatureQuantiles:
+    print(k, lExplainer.mImplementation.mFeatureQuantiles[k])
 
