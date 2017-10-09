a1452d5ae140863d7087fd6db581d72d8057c383
Antoine Carme <antoine.carme@laposte.net>
2017-10-09 10:50:16 +0200

    Add a way to show local scorecards #9 Corrcetd a typo

--- explainer.py
+++ explainer.py
@@ -2,7 +2,7 @@ from sklearn_explain.reason_codes import explain_factory as expl
 from sklearn_explain.reason_codes import settings as conf
 
 # This is a model explainer designed for classifiers and regressors with continous inputs.
-# Support for multiclass models and categorical inputs is coming.
+# Support for multiclass models and categorical inputs is also available.
 # usage :
 
 '''
@@ -15,20 +15,25 @@ class cModelScoreExplainer:
 
     def __init__(self , clf):
         self.mModel = clf
-        self.mImplemantation = None
+        self.mImplementation = None
         self.mSettings = conf.cScoreExplainerConfig()
         self.mDebug = True
 
 
     def fit(self, X):
         lFactory = expl.cScoreExplainerFactory()
-        self.mImplemantation = lFactory.build_Explainer(self.mModel , self.mSettings)
+        self.mImplementation = lFactory.build_Explainer(self.mModel , self.mSettings)
         if(hasattr(self , "mFeatureNames")):
-            self.mImplemantation.mFeatureNames = self.mFeatureNames
-        return self.mImplemantation.fit(X)
+            self.mImplementation.mFeatureNames = self.mFeatureNames
+        return self.mImplementation.fit(X)
 
 
     def explain(self, X):
-        assert(self.mImplemantation is not None)
-        return self.mImplemantation.explain(X)
+        assert(self.mImplementation is not None)
+        return self.mImplementation.explain(X)
     
+
+    def get_local_score_card(self, X):
+        assert(self.mImplementation is not None)
+        return self.mImplementation.get_local_score_card(X)
+        
