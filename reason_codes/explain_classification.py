de87ed14ee95c6c0d260df3127371167a4f0b8f9
Antoine Carme <antoine.carme@laposte.net>
2017-10-09 10:51:56 +0200

    Added report_score_type

--- reason_codes/explain_classification.py
+++ reason_codes/explain_classification.py
@@ -8,19 +8,25 @@ class cClassificationModel_ScoreExplainer(exp.cAbstractScoreExplainer):
 
     def __init__(self , clf, settings = None):
         exp.cAbstractScoreExplainer.__init__(self, clf, settings);
+        self.report_score_type()
+
+    def report_score_type(self):
+        if(hasattr(self.mClassifier , 'predict_proba')):
+            if(self.mSettings.mDebug):
+                print("USING_LOG_ODDS_AS_SCORE")
+        elif(hasattr(self.mClassifier , 'decision_function')):
+            if(self.mSettings.mDebug):
+                print("USING_DECISION_FUNCTION_AS_SCORE")
+        pass
 
     def get_score(self, X):
         class_idx = self.mSettings.get_class_index()
         if(hasattr(self.mClassifier , 'predict_proba')):
-            if(self.mSettings.mDebug):
-                print("USING_PROBABILITY_AS_SCORE")
             lProba = self.mClassifier.predict_proba(X)[:,class_idx]
             lProba = lProba.clip(0.00001 , 0.99999)
             lOdds = lProba / (1.0 - lProba)
             return np.log(lOdds)
         if(hasattr(self.mClassifier , 'decision_function')):
-            if(self.mSettings.mDebug):
-                print("USING_DECISION_FUNCTION_AS_SCORE")
             lDecision = self.mClassifier.decision_function(X)
             if(len(lDecision.shape) == 1):
                 # binary classifier : RidgeClassifier and SGDClassifier
