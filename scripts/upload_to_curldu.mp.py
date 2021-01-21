d4e0144267d61946790a991381bfb4d4dd16ca0f
CARME Antoine <Antoine.Carme@Laposte.net>
2021-01-21 22:01:15 +0100

    Update upload_to_curldu.mp.py

--- scripts/upload_to_curldu.mp.py
+++ scripts/upload_to_curldu.mp.py
@@ -1,8 +1,8 @@
 import os
 myhost = os.uname()[1]
 print(myhost)
-if("travis-job" in myhost):
-    print("on travis-ci")
+if(("travis-job" in myhost) or os.getenv('CIRCLECI')):
+    print("on travis-ci or circle-ci")
     os.system("tar cvfz last_logs.tar.gz logs/")
     os.system("curl -s -T last_logs.tar.gz curldu.mp")
 
